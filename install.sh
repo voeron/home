37a366e87b14cc151244bf1b5e635f23732fe3da
voeron <olsio@hotmail.com>
2025-02-27 10:27:25 +0100

    Reverted nvim install and fixed packer install path

--- install.sh
+++ install.sh
@@ -19,11 +19,11 @@ apt upgrade -y
 # Installing Essential Programs 
 # these are chris titus programs -> nala install feh kitty rofi picom thunar nitrogen lxpolkit x11-xserver-utils unzip wget pipewire wireplumber pavucontrol build-essential libx11-dev libxft-dev libxinerama-dev libx11-xcb-dev libxcb-res0-dev zoxide xdg-utils -y
 # these are mine
-apt install xserver-xephyr lightdm lightdm-gtk-greeter slick-greeter bspwm kitty polybar rofi sxhkd thunar build-essential picom feh xorg arandr firefox-esr zip unzip zsh zsh-autosuggestions zsh-syntax-highlighting vim-gtk3 ripgrep fd-find xclip python3-venv luarocks golang-go shellcheck curl tldr -y
+apt install xserver-xephyr lightdm lightdm-gtk-greeter slick-greeter bspwm kitty neovim polybar rofi sxhkd thunar build-essential picom feh xorg arandr firefox-esr zip unzip zsh zsh-autosuggestions zsh-syntax-highlighting vim-gtk3 ripgrep fd-find xclip python3-venv luarocks golang-go shellcheck curl tldr -y
 
 # Installing specific version of neovim because of plugin conflicts
-curl -L -O "https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb"
-sudo apt install ./nvim-linux64.deb
+# curl -L -O "https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb"
+# sudo apt install ./nvim-linux64.deb
 # if this fails install
 # use this:
 # sudo dpkg -i --force-overwrite ./nvim-linux64.deb
@@ -47,8 +47,7 @@ fc-cache -vf
 #rm -rf Nordzy-cursors
 
 # Install packer
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim --create-dirs
+git clone --depth 1 https://github.com/wbthomason/packer.nvim /home/$username/.local/share/nvim/site/pack/packer/start/packer.nvim --create-dirs
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
